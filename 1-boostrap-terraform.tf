defe66279ba520b12fbd769365f2ae4852ca28c8
Arnaud Morin <arnaud.morin@corp.ovh.com>
2017-04-14 17:37:16 +0200

    Use vrack, its better

--- 1-boostrap-terraform.tf
+++ 1-boostrap-terraform.tf
@@ -3,20 +3,89 @@
 provider "openstack" {
 }
 
-# Create a web server
-resource "openstack_compute_instance_v2" "backends" {
-  name          = "${format("backend-%02d", count.index+1)}"
-  count         = "${var.backends}"
-  region        = "${var.region}"
-  image_name    = "Debian 8"
-  key_pair      = "arnaud-ovh"
-  flavor_name   = "c2-7"
-  network       = { name = "Ext-Net" }
-  connection    = { user = "debian" }
-  user_data     = "${file("backend.yaml")}"
+#
+# SSH key
+#
+resource "openstack_compute_keypair_v2" "keypairs" {
+  name          = "cat-arnaud-ovh"
+  count         = "${length(var.regions)}"
+  region        = "${element(var.regions, count.index)}"
+  public_key    = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDOYsxhRRwC4+uKhJLuoPpNhf1jD97R/S5KnegmCF45QxnSbwfJq4XxBMLc8+BOqP1HLAIRW6HfdZMxkpVVi0aVdYSjJdtBA/9ImceqB0uvXP8Zhsivmr+/u/PB30bUS1/yblOAT62Hxu17KYv79g1FhJa3sJNQ5InzgDOUmLMCYczacMKpVQUFoj92zO3HSe0SXGSBh4++So4rcvD5Dywz6bmMbM/mtzSym95D53RwsJ/9a3CQa3BqIqoRNX/zjGS4C7tpqi1AlwFyBjJzOjqjwDYcTfune22DLnotMX06Salnhp1MZHuhffyUdu8Bkk3RjMiVtTwOECZGr59Ovj5aE2J/xPeDiCWg1J/ois0Zl4TeVipgWL9p/ULBtVS4LpFHO2GKy10rQfW10oYgclR+gfM9SEq3ZuKZmXHieNDUogHCW1oZRi1M3Z7hJEe6C5dmerx02cXb1CBp1OebmrKxQMBt8QtWpnKEE+74GEUbjyRI8oDGOzdTPhkvH6M1o7WyyWmxLwtFkuQkaKxa5lGju+LXJb9tnAyijjpH1h0BldWuEdfiijuIaRieDj1CBDDnn9Ukcjl1eYqGC0KX3M4rq5EIMHwZ8Nb4Lrb6SqUKWz2RYKKWIsT3rJFa3jKiUVzSODM8cGf0rFTKqXihCdqvfkdAYchKli1U6xaEZdl0zw=="
 }
 
-# Create template for frontend
+#
+# Private networking
+#
+# Create the subnets
+resource "openstack_networking_subnet_v2" "subnets" {
+  name          = "subnet_${element(var.regions, count.index)}"
+  count         = "${length(var.regions)}"
+  network_id    = "${element(var.networks, count.index)}"
+  cidr          = "192.168.1.0/24"
+  region        = "${element(var.regions, count.index)}"
+  no_gateway    = "true"
+  enable_dhcp   = "true"
+}
+
+# Create a router, to give backends access to internet, through NAT
+resource "openstack_compute_instance_v2" "router" {
+  name          = "router"
+  region        = "SBG3"
+  image_name    = "Debian 8"
+  key_pair      = "cat-arnaud-ovh"
+  flavor_name   = "c2-7"
+
+  # Network configuration
+  # This VM has double attachement:
+  #  - one public interface to internet
+  #  - one private interface to communicate with backends
+  network       = { name = "Ext-Net" }
+  network       = {
+    name        = "VLAN"
+    fixed_ip_v4 = "192.168.1.1"
+  }
+
+  # Postinstall
+  connection    = { user = "debian" }
+  user_data     = "${file("router.yaml")}"
+}
+
+#
+# BACKENDS
+#
+# Create one backend per region
+resource "openstack_compute_instance_v2" "backends" {
+  name          = "${format("backend-%02d", count.index+1)}"
+  count         = "${length(var.regions)}"
+  region        = "${element(var.regions, count.index)}"
+  image_name    = "Debian 8"
+  key_pair      = "cat-arnaud-ovh"
+  flavor_name   = "c2-7"
+
+  # Network configuration
+  # This VM has only one interface:
+  #  - private interface to communicate with frontend
+  # Direct access from internet is not possible
+  # Access from VM to internet is possible thanks to router (NAT)
+  network       = {
+    name        = "VLAN"
+    fixed_ip_v4 = "192.168.1.1${count.index+1}"
+  }
+
+  # Postinstall
+  connection    = { user = "debian" }
+  user_data     = "${file("backend.yaml")}"
+
+  # Ordering
+  # We need router before spawning the backends
+  depends_on    = ["openstack_compute_instance_v2.router"]
+}
+
+#
+# FRONTEND
+#
+# Create template for frontend user_data
+# The user_data is filled with backends IP before spawning the frontend
 data "template_file" "frontend_user_data" {
   template = "${file("frontend.yaml.tpl")}"
   vars {
@@ -24,18 +93,29 @@ data "template_file" "frontend_user_data" {
   }
 }
 
-# Create a frontend server
+# Create one frontend
 resource "openstack_compute_instance_v2" "frontend" {
   name          = "frontend"
-  region        = "${var.region}"
+  region        = "SBG3"
   image_name    = "Debian 8"
-  key_pair      = "arnaud-ovh"
+  key_pair      = "cat-arnaud-ovh"
   flavor_name   = "c2-7"
+
+  # Network configuration
+  # This VM has double attachement:
+  #  - one public interface to be reachable from internet
+  #  - one private interface to communicate with backends
   network       = { name = "Ext-Net" }
+  network       = { name = "VLAN" }
+
+  # Postinstall
   connection    = { user = "debian" }
   user_data     = "${data.template_file.frontend_user_data.rendered}"
 }
 
+#
+# OUTPUT
+#
 output "backends" {
   value = "${join("\n", formatlist("http://%s:5000", openstack_compute_instance_v2.backends.*.access_ip_v4))}"
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,15 @@
-variable "backends" {
-  default = "2"
+variable "regions" {
+  default = [
+    "SBG3",
+    "BHS3",
+    "GRA3"
+  ]
 }
-variable "region" {
-  default = "SBG3"
+
+variable "networks" {
+  default = [
+    "abf47637-3e12-4c48-9ba2-8a7884a55311",
+    "59c39165-12aa-4b7b-a7ab-e76a153741a9",
+    "3dfc5b5c-9453-48ad-8cfb-a6f1c489b09c",
+  ]
 }
