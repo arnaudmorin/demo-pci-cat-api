cfe1699a34b45e2508111d7ac8d585472a6ac1eb
Arnaud Morin <arnaud.morin@corp.ovh.com>
2017-04-18 10:07:46 +0200

    Rework gif

--- 0-boostrap-backend-only.sh
+++ 0-boostrap-backend-only.sh
@@ -1,1 +1,1 @@
-openstack server create --key-name arnaud-ovh --flavor c2-7 --image 'Debian 8' --nic net-name=Ext-Net --user-data backend.yaml --wait cat-api
+openstack server create --key-name arnaud-ovh --flavor c2-7 --image 'Debian 8' --nic net-id=Ext-Net --user-data backend.yaml --wait cat-api
