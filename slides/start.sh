642ca4fb03da773ff2f49b92bd12f5f214af63bf
Arnaud Morin <arnaud.morin@corp.ovh.com>
2017-09-28 17:57:31 +0200

    Update slides

--- slides/start.sh
+++ slides/start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-python -m SimpleHTTPServer 8000
+[ $(python --version | awk '{print $2}' | awk -F'.' '{print $1}') -eq 3 ] && \
+    python -m http.server 8000 \
+|| \
+    python -m SimpleHTTPServer 8000
